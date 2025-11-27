3067651e5494e39d00c47bd8df7512e26da781ab
Hsaraujo-98 <hsaraujo98@outlook.com>
2025-11-27 18:22:43 +0000

    0. Criando o primeiro Microsserviço (endpoint) com SpringBoot usando Spring Tool Suite 4

---
+++ 0x01/spring_boot_1/demo/src/main/java/com/example/demo/MessageResource.java
@@ -0,0 +1,29 @@
+
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/messages")
+public class MessageResource {
+
+    @GetMapping("/simpleMessageWelcome")
+    public String simpleMessageWelcome() {
+        return "BEM VINDO A AULA DE MICROSSERVIÇO USANDO SPRING BOOT !!!";
+    }
+
+    @GetMapping("/login")
+    public String login(@RequestParam(required = false) String user,
+            @RequestParam(required = false) String password) {
+
+        if (user == null || user.isBlank() || password == null || password.isBlank()) {
+            return "USUÁRIO E SENHA NÃO INFORMADOS";
+        }
+
+        if (user.length() > 15 || password.length() > 15) {
+            return "USUÁRIO E SENHA INVÁLIDOS";
+        }
+
+        return "LOGIN EFETUADO COM SUCESSO !!!";
+    }
+}
